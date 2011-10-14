bbec474d8b38dfc0ce2afe8927b81a3c1a8de498
Alexander Troshchenko <flashvw@mail.ru>
2011-10-14 14:16:05 +0400

    sh commit

--- git-test/src/tmp/HellWorldGit.java
+++ git-test/src/tmp/HellWorldGit.java
@@ -4,5 +4,6 @@ public class HellWorldGit {
 	public static void main(String[] args) {
 		System.out.println("tralivali");
 		System.out.println("lalatopola");
+		System.out.println("shuser");
 	}
 }
